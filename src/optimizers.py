8fda4e8d11db17b175821b96bebba3b432122c51
IssamLaradji <issam.laradji@gmail.com>
2020-04-14 10:50:17 +0000

    removed outdated adapt functions

--- src/optimizers.py
+++ src/optimizers.py
@@ -14,7 +14,7 @@ def get_optimizer(opt_dict, params, n_batches_per_epoch=None):
     # our optimizers   
     n_batches_per_epoch = opt_dict.get("n_batches_per_epoch") or n_batches_per_epoch    
     if opt_name == 'sps':
-        opt = sps(params, c=opt_dict["c"], 
+        opt = SPS(params, c=opt_dict["c"], 
                         n_batches_per_epoch=n_batches_per_epoch, 
                         adapt_flag=opt_dict.get('adapt_flag'),
                         fstar_flag=opt_dict.get('fstar_flag'),
@@ -41,8 +41,7 @@ def get_optimizer(opt_dict, params, n_batches_per_epoch=None):
 
     return opt
 
-
-class sps(torch.optim.Optimizer):
+class SPS(torch.optim.Optimizer):
     def __init__(self,
                  params,
                  n_batches_per_epoch=500,
@@ -71,19 +70,13 @@ class sps(torch.optim.Optimizer):
 
         self.state['n_forwards'] = 0
         self.state['n_backwards'] = 0
-        self.state['lb'] = None
-        self.loss_min = np.inf
-        self.loss_sum = 0.
-        self.loss_max = 0.
         self.fstar_flag = fstar_flag
 
     def step(self, closure, batch):
-        indices = batch['meta']['indices']
-        # deterministic closure
-        seed = time.time()
+        # increment step
+        self.state['step'] += 1
 
-        batch_step_size = self.state['step_size']
-
+        # get fstar
         if self.fstar_flag:
             fstar = float(batch['meta']['fstar'].mean())
         else:
@@ -93,108 +86,49 @@ class sps(torch.optim.Optimizer):
         loss = closure()
         loss.backward()
 
-        # increment # forward-backward calls
-        self.state['n_forwards'] += 1
-        self.state['n_backwards'] += 1
-
-        if self.state['step'] % int(self.n_batches_per_epoch) == 0:
-            self.state['step_size_avg'] = 0.
-
-        self.state['step'] += 1
-
         # save the current parameters:
         params_current = copy.deepcopy(self.params)
         grad_current = get_grad_list(self.params)
 
         grad_norm = compute_grad_norm(grad_current)
         
-        if grad_norm < 1e-6:
-            return 0.
-
-        if self.adapt_flag == 'moving_lb':
-            if (self.state['lb'] is not None) and loss > self.state['lb']: 
-                step_size = (loss - self.state['lb']) / (self.c * (grad_norm)**2)
-                step_size = step_size.item()
-                loss_scalar = loss.item()
-            else:
-                step_size = 0.
-                loss_scalar = loss.item()
-
-            if (self.state['step'] % int(self.n_batches_per_epoch)) == 0:
-                self.state['lb'] = self.loss_min / 100.
-
-                print('lower_bound:', self.state['lb'])
-
-            self.loss_sum += loss_scalar
-            self.loss_min = min(loss_scalar, self.loss_min)
-            self.loss_max = max(loss_scalar, self.loss_max)
-
-        elif self.adapt_flag in ['constant']:
+        # adapt the step size
+        if self.adapt_flag in ['constant']:
+            # adjust the step size based on an upper bound and fstar
             step_size = (loss - fstar) / (self.c * (grad_norm)**2 + self.eps)
             if loss < fstar:
                 step_size = 0.
-                loss_scalar = 0.
             else:
                 if self.eta_max is None:
                     step_size = step_size.item()
                 else:
                     step_size =  min(self.eta_max, step_size.item())
                     
-                loss_scalar = loss.item()
-
-        elif self.adapt_flag in ['basic']:
-            if torch.isnan(loss):
-                raise ValueError('loss is NaN')
-            # assert(loss >= fstar)
-            if loss < fstar:
-                step_size = 0.
-                loss_scalar = 0.
-            else:
-                step_size = (loss - fstar) / (self.c * (grad_norm)**2+ self.eps)
-                step_size =  step_size.item()
-
-                loss_scalar = loss.item()
-
         elif self.adapt_flag in ['smooth_iter']:
+            # smoothly adjust the step size
             step_size = loss / (self.c * (grad_norm)**2)
             coeff = self.gamma**(1./self.n_batches_per_epoch)
             step_size =  min(coeff * self.state['step_size'], 
                              step_size.item())
            
-            loss_scalar = loss.item()
-
-        elif self.adapt_flag == 'smooth_epoch':
-            step_size = loss / (self.c * (grad_norm)**2)
-            step_size = step_size.item()
-            if self.state['step_size_epoch'] != 0:
-                step_size =  min(self.state['step_size_epoch'], 
-                                 step_size)
-                self.step_size_max = max(self.step_size_max, step_size)
-            else:
-                self.step_size_max = max(self.step_size_max, step_size)
-                step_size = 0.
-                
-            loss_scalar = loss.item()
-
-             # epoch done
-            if (self.state['step'] % int(self.n_batches_per_epoch)) == 0:
-                self.state['step_size_epoch'] = self.step_size_max
-                self.step_size_max = 0.
-
-        # update
+        # update with step size
         try_sgd_update(self.params, step_size, params_current, grad_current)
 
-        # save the new step-size
-        self.state['step_size'] = step_size
-
+        # update state with metrics
+        if self.state['step'] % int(self.n_batches_per_epoch) == 1:
+            # reset step size avg for each new epoch
+            self.state['step_size_avg'] = 0.
         self.state['step_size_avg'] += (step_size / self.n_batches_per_epoch)
+
+        self.state['n_forwards'] += 1
+        self.state['n_backwards'] += 1
+        self.state['step_size'] = step_size
         self.state['grad_norm'] = grad_norm.item()
         
         if torch.isnan(self.params[0]).sum() > 0:
-            print('nan')
+            raise ValueError('Got NaNs')
 
-        return loss
-
+        return float(loss)
 
 # utils
 # ------------------------------
