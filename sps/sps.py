c74f5aee054939ba428197b51837663dd4701640
Simon Ramstedt <simonramstedt@gmail.com>
2020-04-22 03:20:05 -0400

    make_sps_more_canonical_and_fix_pickling

--- sps/sps.py
+++ sps/sps.py
@@ -1,7 +1,7 @@
-import numpy as np
-import torch
 import time
 import copy
+import numpy as np
+import torch
 
 
 class Sps(torch.optim.Optimizer):
@@ -11,96 +11,89 @@ class Sps(torch.optim.Optimizer):
                  init_step_size=1,
                  c=0.5,
                  gamma=2.0,
-                 eta_max=None,
+                 eta_max=10,
                  adapt_flag='smooth_iter',
                  fstar_flag=None,
                  eps=1e-8):
-        params = list(params)
-        super().__init__(params, {})
-        self.eps = eps
-        self.params = params
-        self.c = c
-        self.eta_max = eta_max
-        self.gamma = gamma
-        self.init_step_size = init_step_size
-        self.adapt_flag = adapt_flag
-        self.state['step'] = 0
-
-        self.state['step_size'] = init_step_size
-        self.step_size_max = 0.
-        self.n_batches_per_epoch = n_batches_per_epoch
-
-        self.state['n_forwards'] = 0
-        self.state['n_backwards'] = 0
-        self.fstar_flag = fstar_flag
+        defaults = {k: v for k,v in locals().items() if k not in ('params',)}
+        super().__init__(params, defaults)
 
     def step(self, closure=None, loss=None, batch=None):
         if loss is None and closure is None:
             raise ValueError('please specify either closure or loss')
 
-        if loss is not None:
-            if not isinstance(loss, torch.Tensor):
-                loss = torch.tensor(loss)
-
-        # increment step
-        self.state['step'] += 1
-
-        # get fstar
-        if self.fstar_flag:
-            fstar = float(batch['meta']['fstar'].mean())
-        else:
-            fstar = 0.
-
         # get loss and compute gradients
         if loss is None:
             loss = closure()
         else:
-            assert closure is None, 'if loss is provided then closure should beNone'
+            assert closure is None, 'if loss is provided then closure should be None'
+            loss = torch.as_tensor(loss)
 
-        # save the current parameters:
-        params_current = copy.deepcopy(self.params)
-        grad_current = get_grad_list(self.params)
-        grad_norm = compute_grad_norm(grad_current)
+        for group in self.param_groups:
+            # more initializations
+            if 'step' not in group:
+                group['step'] = 0
+                group['step_size_avg'] = 0.
+                group['step_size'] = group['init_step_size']
+                group['n_forwards'] = 0
+                group['n_backwards'] = 0
 
-        if grad_norm < 1e-8:
-            step_size = 0.
-        else:
-            # adapt the step size
-            if self.adapt_flag in ['constant']:
-                # adjust the step size based on an upper bound and fstar
-                step_size = (loss - fstar) / \
-                    (self.c * (grad_norm)**2 + self.eps)
-                if loss < fstar:
-                    step_size = 0.
-                else:
-                    if self.eta_max is None:
-                        step_size = step_size.item()
-                    else:
-                        step_size = min(self.eta_max, step_size.item())
+            # increment step
+            group['step'] += 1
 
-            elif self.adapt_flag in ['smooth_iter']:
-                # smoothly adjust the step size
-                step_size = loss / (self.c * (grad_norm)**2 + self.eps)
-                coeff = self.gamma**(1./self.n_batches_per_epoch)
-                step_size = min(coeff * self.state['step_size'],
-                                step_size.item())
+            # get fstar
+            if group['fstar_flag']:
+                fstar = float(batch['meta']['fstar'].mean())
             else:
-                raise ValueError('adapt_flag: %s not supported' %
-                                 self.adapt_flag)
+                fstar = 0.
 
-            # update with step size
-            sgd_update(self.params, step_size, params_current, grad_current)
+            # save the current parameters:
+            params_current = copy.deepcopy(group['params'])
+            grad_current = get_grad_list(group['params'])
+            grad_norm = compute_grad_norm(grad_current)
 
-        # update state with metrics
-        self.state['n_forwards'] += 1
-        self.state['n_backwards'] += 1
-        self.state['step_size'] = step_size
-        self.state['grad_norm'] = grad_norm.item()
+            if grad_norm < 1e-8:
+                step_size = 0.
+            else:
+                # adapt the step size
+                if group['adapt_flag'] in ['constant']:
+                    # adjust the step size based on an upper bound and fstar
+                    step_size = (loss - fstar) / (group['c'] * (grad_norm)**2 + group['eps'])
+                    if loss < fstar:
+                        step_size = 0.
+                    else:
+                        if group['eta_max'] is None:
+                            step_size = step_size.item()
+                        else:
+                            step_size = min(group['eta_max'], step_size.item())
 
-        if torch.isnan(self.params[0]).sum() > 0:
-            raise ValueError('Got NaNs')
+                elif group['adapt_flag'] in ['smooth_iter']:
+                    # smoothly adjust the step size
+                    step_size = loss / (group['c'] * (grad_norm)**2 + group['eps'])
+                    coeff = group['gamma']**(1./group['n_batches_per_epoch'])
+                    step_size = min(coeff * group['step_size'], step_size.item())
+                else:
+                    raise ValueError('adapt_flag: %s not supported' % group['adapt_flag'])
 
-        return float(loss)
+                # update with step size
+                sgd_update(group['params'], step_size,params_current, grad_current)
+
+            # update state with metrics
+            if group['step'] % int(group['n_batches_per_epoch']) == 1:
+                # reset step size avg for each new epoch
+                group['step_size_avg'] = 0.
+
+            group['step_size_avg'] += (step_size / group['n_batches_per_epoch'])
+
+            group['n_forwards'] += 1
+            group['n_backwards'] += 1
+            group['step_size'] = step_size
+            group['grad_norm'] = grad_norm.item()
+
+            if torch.isnan(group['params'][0]).sum() > 0:
+                raise ValueError('Got NaNs')
+
+        return loss
 
 # utils
 # ------------------------------
@@ -121,7 +114,6 @@ def get_grad_list(params):
 
 
 def sgd_update(params, step_size, params_current, grad_current):
-    zipped = zip(params, params_current, grad_current)
-
-    for p_next, p_current, g_current in zipped:
-        p_next.data = p_current - step_size * g_current
+    for p_next, p_current, g_current in zip(params, params_current, grad_current):
+        if g_current is not None:
+            p_next.data = p_current - step_size * g_current
